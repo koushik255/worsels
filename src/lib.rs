0bf6d1843b590ef49379ae17c9de288837d50f19
koushik <191687017+koushik255@users.noreply.github.com>
2025-06-23 13:55:37 -0400

    supdude

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,29 @@
+
 use clap::{command,Arg};
 use std::fs;
 use std::io;
 use colored::*;
 
-pub fn input() -> String {
+pub fn input() -> bool {
     let matches = command!()
-        .arg(Arg::new("input").help("your help input").required(true))
-        .get_matches();
+        .arg(
+            Arg::new("input")
+            .short('k')
+            .long("force")
+         .help("your help input")
+         .required(true)
+         .action(clap::ArgAction::SetTrue),
+    )
+    .get_matches();
 
-    let input = matches
-        .get_one::<String>("input")
-        .expect("input is required")
-        .trim();
-    input.to_string()
+    let input = matches.get_flag("input");
+    input
+    
+
 }
 
-pub fn match_and_check(input: Option<String>) {
-    match input {
-        Some(input) => {
-            println!("Here was your input {:?} ", input);
-        },
-        None => {
-            println!("No input was given");
-        }
-    }
-}
+
+
 
 pub fn list_files() -> io::Result<Vec<String>> {
     fs::read_dir(".")?
@@ -35,7 +34,8 @@ pub fn list_files() -> io::Result<Vec<String>> {
 pub fn check_file_type(){
     let files = list_files().unwrap();
 
-    for file in files.iter() {
+    for file in files.iter()
+        .filter(|line| !line.contains("/.")){
         // maybe change to foreach
         match fs::metadata(file) {
             Ok(metadata) => {
@@ -55,14 +55,43 @@ pub fn check_file_type(){
         }
     }
 }
+
+
+pub fn find_hid() {
+    let files = match list_files() {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error listing hidden files: {}", e);
+            return;
+        }
+    };
+
+    // dont use enumerate when filtering it fucks everythign up
+    let hidden_files: Vec<&String> = files
+        .iter()
+        .filter(|file_path| file_path.contains("/.")) //
+        .collect();
+
+
+    for file_path in hidden_files {
+        println!("HID {}", file_path);
+    }
+}
+  
 // if file is hidden (has a ./. and is a dir then its hidden
-// make it so you put a flag like --h and then it it will
+// make it so you put a flag like -h and then it it will
 // show the hidden files
 
 // make it so the file size is also a flag 
 
+
+
+// to run use cargo run -- -k 
+//
+
+
 pub fn run() {
-    let input = input();
-    match_and_check(Some(input));
+    find_hid();
+    
     check_file_type();
 }
